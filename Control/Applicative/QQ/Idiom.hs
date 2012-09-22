c5a154277803a0dd12971a572792407d24556889
Ben Millwood <thebenmachine+git@gmail.com>
2012-09-22 17:14:58 +0100

    Haddock fixes/updates in Idiom.hs

--- Control/Applicative/QQ/Idiom.hs
+++ Control/Applicative/QQ/Idiom.hs
@@ -11,22 +11,22 @@ import Language.Haskell.TH.Lib
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
 
--- | Turns function application into <*>, and puts a pure on the beginning.
+-- | Turns function application into '<*>', and puts a 'pure' on the beginning.
 --
--- [i| subtract [1,2,3] [10,20,30] |]
--- -> pure subtract <*> [1,2,3] <*> [10,20,30]
--- -> [99,199,299,98,198,298,97,197,297]
+-- > [i| subtract [1,2,3] [10,20,30] |]
+-- > -> pure subtract <*> [1,2,3] <*> [10,20,30]
+-- > -> [9,19,29,8,18,28,7,17,27]
 --
 -- Does not apply to nested applications:
 --
--- getZipList [i| subtract (ZipList [1,2,3]) (ZipList [100,200,300]) |]
--- -> getZipList (pure subtract <*> ZipList [1,2,3] <*> ZipList [100,200,300])
--- -> [99,198,297]
+-- > getZipList [i| subtract (ZipList [1,2,3]) (ZipList [10,20,30]) |]
+-- > -> getZipList (pure subtract <*> ZipList [1,2,3] <*> ZipList [10,20,30])
+-- > -> [9,18,27]
 --
--- Will treat [i| x `op` y |] as [i| op x y |] as long as neither x nor y
--- are an infix expression. The precise behaviour when x or y are infix
--- applications depends on what haskell-src-meta does, which depends on what
--- TH supports, so may depend on your GHC version.
+-- Will treat @[i| x \`op\` y |]@ as @[i| op x y |]@ as long as neither x nor y
+-- are an infix expression. If they are, will likely complain that it doesn't
+-- have fixity information (unless haskell-src-meta becomes clever enough to
+-- resolve that itself).
 i :: QuasiQuoter
 i = QuasiQuoter { quoteExp = applicate <=< either fail return . parseExp,
   quotePat = nonsense "pattern",
